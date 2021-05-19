8fac4eb825412ef7453fa5ec60369ab4feb138e7
adearmas21 <61759272+adearmas21@users.noreply.github.com>
2021-05-19 13:41:42 +0200

    no hice nada

--- PracticaFinalPC/src/Cliente.java
+++ PracticaFinalPC/src/Cliente.java
@@ -3,7 +3,6 @@ import Main.Fichero;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.Socket;
-import java.io.IOException;
 import java.io.InputStream;
 
 public class Cliente {
@@ -18,7 +17,7 @@ public class Cliente {
     private static Socket socket;
 
     public static void main(String[] args) throws IOException {
-        //cin>>x;
+        
   
         ObjectInputStream objectInput = new ObjectInputStream(socket.getInputStream());//Servidor/Fichero
       
@@ -28,25 +27,19 @@ public class Cliente {
         /*
           - leer nombre teclado 
           - crear socket con servidor 
-<<<<<<< HEAD
           - crear nuevo thread OyenteServidor para leer el socket 
           - enviar MENSAJE_CONEXION 
           - establecer  menu con usuario: 
           		consultar lista usuarios: enviar MENSAJE_LISTA_USUARIOS
           -pedir fichero enviar MENSAJE_PEDIR_FICHERO 3 
           -salir enviar MENSAJE_CERRAR_CONEXION
-=======
           - crear nuevo thread
           OyenteServidor para leer el socket
            - enviar MENSAJE_CONEXION - establecer
           menu con usuario: consultar lista usuarios: enviar MENSAJE_LISTA_USUARIOS
           -pedir fichero enviar MENSAJE_PEDIR_FICHERO 3 -salir enviar
           MENSAJE_CERRAR_CONEXION
->>>>>>> refs/remotes/origin/main
-          
-         */
-
-        
+*/   
 
     }
 }
